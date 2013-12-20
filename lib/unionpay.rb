483ad217a792239a9699738e92f9bf8cdaa18b10
kikyous <kikyous@163.com>
2013-12-20 16:55:28 +0800

    add  more test

--- lib/unionpay.rb
+++ lib/unionpay.rb
@@ -18,19 +18,20 @@ module UnionPay
       case e
       ## 测试环境
       when :development
-        UnionPay.front_pay_url = "http://58.246.226.99/UpopWeb/api/Pay.action"
-        UnionPay.back_pay_url = "http://58.246.226.99/UpopWeb/api/BSPay.action"
-        UnionPay.query_url = "http://58.246.226.99/UpopWeb/api/Query.action"
+        self.front_pay_url = "http://58.246.226.99/UpopWeb/api/Pay.action"
+        self.back_pay_url = "http://58.246.226.99/UpopWeb/api/BSPay.action"
+        self.query_url = "http://58.246.226.99/UpopWeb/api/Query.action"
       ## 预上线环境
       when :pre_production
-        UnionPay.front_pay_url = "https://www.epay.lxdns.com/UpopWeb/api/Pay.action"
-        UnionPay.back_pay_url = "https://www.epay.lxdns.com/UpopWeb/api/BSPay.action"
-        UnionPay.query_url = "https://www.epay.lxdns.com/UpopWeb/api/Query.action"
+        self.front_pay_url = "https://www.epay.lxdns.com/UpopWeb/api/Pay.action"
+        self.front_pay_url = "http://58.246.226.99/UpopWeb/api/Pay.action"
+        self.back_pay_url = "https://www.epay.lxdns.com/UpopWeb/api/BSPay.action"
+        self.query_url = "https://www.epay.lxdns.com/UpopWeb/api/Query.action"
       ## 线上环境
       else
-        UnionPay.front_pay_url = "https://unionpaysecure.com/api/Pay.action"
-        UnionPay.back_pay_url = "https://besvr.unionpaysecure.com/api/BSPay.action"
-        UnionPay.query_url = "https://query.unionpaysecure.com/api/Query.action"
+        self.front_pay_url = "https://unionpaysecure.com/api/Pay.action"
+        self.back_pay_url = "https://besvr.unionpaysecure.com/api/BSPay.action"
+        self.query_url = "https://query.unionpaysecure.com/api/Query.action"
       end
     end
   end
--- test/unionpay/service_test.rb
+++ test/unionpay/service_test.rb
@@ -1,8 +1,7 @@
 require 'test_helper'
 
 class UnionPay::ServiceTest < Test::Unit::TestCase
-
-  def test_generate_form
+  def generate_form
     param = {}
     param['transType']     = UnionPay::CONSUME                         #交易类型，CONSUME or PRE_AUTH
     param['orderAmount']   = 11000                                           #交易金额
@@ -12,8 +11,19 @@ class UnionPay::ServiceTest < Test::Unit::TestCase
     param['backEndUrl']    = "http://www.example.com/sdk/utf8/back_notify.php"     #后台回调URL
     param['orderTime']     = '20131220151706'
     param['orderCurrency'] = UnionPay::CURRENCY_CNY                    #交易币种，CURRENCY_CNY=>人民币
-    service = UnionPay::Service.front_pay(param)
-    form = service.form(target: '_blank', id: 'form'){"<input type='submit' />"}
-    assert_not_nil form
+    UnionPay::Service.front_pay(param)
   end
+
+  def test_generate_form
+    assert_not_nil generate_form.form(target: '_blank', id: 'form'){"<input type='submit' />"}
+  end
+
+  def test_generate_form_with_different_environment
+    UnionPay.environment = :development
+    dev_form = generate_form.form(target: '_blank', id: 'form'){"<input type='submit' />"}
+    UnionPay.environment = :production
+    pro_form = generate_form.form(target: '_blank', id: 'form'){"<input type='submit' />"}
+    assert dev_form != pro_form
+  end
+
 end
